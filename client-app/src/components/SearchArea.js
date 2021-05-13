cee35575e2b8e7dd09963a4e7496bc5bfa615fc8
Obedia Lawrence <omlawrence24@gmail.com>
2021-05-13 13:40:58 -0400

    api

---
+++ client-app/src/components/SearchArea.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SearchArea = (props) => {
+    return (
+        <div className="search">
+            <form 
+            onSubmit={props.searchBook} 
+            action="">
+        <input 
+           onChange={props.handleSearch}
+            type="text"/>
+        <button type="submit">
+        search
+        </button>
+            </form>
+        
+        </div>
+    )
+}
+
+export default SearchArea
